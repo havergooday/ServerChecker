88a0596f98eb6b8eb87c2a3259bffdf27d5f108b
Raven <havergooday@gmail.com>
2023-04-01 09:21:17 +0900

    초기 컨텐츠 업로드 - json파싱까지했음

---
+++ ServerChecker/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace ServerChecker
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 필수 디자이너 변수입니다.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 사용 중인 모든 리소스를 정리합니다.
+		/// </summary>
+		/// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 디자이너에서 생성한 코드
+
+		/// <summary>
+		/// 디자이너 지원에 필요한 메서드입니다. 
+		/// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnRefresh = new System.Windows.Forms.Button();
+			this.sRefrshTime = new System.Windows.Forms.Label();
+			this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+			this.SuspendLayout();
+			// 
+			// btnRefresh
+			// 
+			this.btnRefresh.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+			this.btnRefresh.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.btnRefresh.Location = new System.Drawing.Point(216, 192);
+			this.btnRefresh.Name = "btnRefresh";
+			this.btnRefresh.Size = new System.Drawing.Size(56, 57);
+			this.btnRefresh.TabIndex = 0;
+			this.btnRefresh.Text = "Refresh";
+			this.btnRefresh.UseVisualStyleBackColor = false;
+			this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+			// 
+			// sRefrshTime
+			// 
+			this.sRefrshTime.AutoSize = true;
+			this.sRefrshTime.Location = new System.Drawing.Point(161, 237);
+			this.sRefrshTime.Name = "sRefrshTime";
+			this.sRefrshTime.Size = new System.Drawing.Size(49, 12);
+			this.sRefrshTime.TabIndex = 1;
+			this.sRefrshTime.Text = "00:00:00";
+			// 
+			// checkedListBox1
+			// 
+			this.checkedListBox1.FormattingEnabled = true;
+			this.checkedListBox1.Location = new System.Drawing.Point(12, 9);
+			this.checkedListBox1.Name = "checkedListBox1";
+			this.checkedListBox1.Size = new System.Drawing.Size(260, 180);
+			this.checkedListBox1.TabIndex = 3;
+			this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+			// 
+			// Form1
+			// 
+			this.ClientSize = new System.Drawing.Size(284, 261);
+			this.Controls.Add(this.checkedListBox1);
+			this.Controls.Add(this.sRefrshTime);
+			this.Controls.Add(this.btnRefresh);
+			this.Name = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnRefresh;
+		private System.Windows.Forms.Label sRefrshTime;
+		private System.Windows.Forms.CheckedListBox checkedListBox1;
+	}
+}
+
---
+++ ServerChecker/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace ServerChecker
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+			sRefrshTime.Text = DateTime.Now.ToString("hh:mm:ss");
+			string folderPath = Application.StartupPath;
+			string path = folderPath + "/ServerList.json";
+			if (!File.Exists(path))
+			{
+				// Create a file to write to.
+				sRefrshTime.Text = "File not Exist:\n" + path;
+			}
+			else
+			{
+				try
+				{
+					List<ServerInfo> servers = JsonConvert.DeserializeObject<List<ServerInfo>>(File.ReadAllText(path));
+					CheckedListBox checkedListBox = new CheckedListBox();
+					for (int i = 0; i < servers.Count; i++)
+					{
+						checkedListBox.Items.Add(servers[i].Name);
+						checkedListBox1.Controls.Add(checkedListBox);
+					}
+				}
+				catch (JsonException ex)
+				{
+					Console.WriteLine($"Error deserializing JSON: {ex.Message}");
+				}
+			}
+		}
+
+		private void btnRefresh_Click(object sender, EventArgs e)
+		{
+			sRefrshTime.Text = DateTime.Now.ToString("hh:mm:ss");
+
+		}
+
+		private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+
+		}
+	}
+
+
+	public class ServerInfo
+	{
+		public string Name { get; set; }
+		public string Url { get; set; }
+	}
+
+}
